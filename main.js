7b57d4104475eea76a82e02a0559ff72a899a299
Veer <80690781+vego-bball@users.noreply.github.com>
2021-09-19 17:42:56 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload()
+{}
+
+function setup()
+{
+    canvas = createCanvas(320, 360);
+    canvas.center();
+}
+
+function take_snapshot()
+{
+    save("Filter.png");
+}
+
+function draw()
+{}
